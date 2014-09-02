c2a4eb77a42a5d686948ff386825ea1901196dca
JDucommun666 <jon.ducommun@gmail.com>
2014-09-02 13:36:29 -0500

    dumb textbook work

--- Chapter 5/username2.py
+++ Chapter 5/username2.py
@@ -1,15 +1,30 @@
 __author__ = 'JDucommun'
 
 def main():
-    print("This program makes usernames.\n")
+    print("This program makes usernames from a file of names.\n")
 
-    #Get their name
-    first = input("Please enter your first name lowercase")
-    last = input("Please enter your last name lowercase")
+    # Get their name
+    infileName = input("What file are their names in? ")
+    outfileName = input("What file should their usernames go in?")
 
-    #concatenate it
-    uname = first[0] + last[:7]
+    # Open the files
+    infile = open(infileName, "r")
+    outfile = open(outfileName, "w")
 
-    print("Your username is: ", uname)
+    # Process each line of the input file
+    for line in infile:
+        # get the first and last names from each line
+        first, last = line.split()
+        # Create the username
+        uname = (first[0] + last[:7]).lower()
+        # write that b!tch
+        print(uname, file=outfile)
+
+    # Close the files
+    infile.close()
+    outfile.close()
+
+
+    print("Usernames built successfully to: ", outfileName)
 
 main()
