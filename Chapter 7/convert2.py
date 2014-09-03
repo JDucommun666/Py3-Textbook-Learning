40d4c1ba12e7b40d2ce838399c9eea4f0b0c5410
JDucommun666 <jon.ducommun@gmail.com>
2014-09-03 13:26:27 -0500

    dumb textbook work

---
+++ Chapter 7/convert2.py
@@ -0,0 +1,16 @@
+__author__ = 'JDucommun'
+
+def main():
+    print("Welcome to convert.py!")
+    print("I take your Celsius and turn it into Fahrenheit.")
+    celsius = eval(input("Enter the temperature in Celsius! "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+    #Warnings
+    if fahrenheit > 90:
+        print("It's really hot out there.  Grab some water!")
+    if fahrenheit < 30:
+        print("Wow.  Nipple Alert!")
+
+main()
