fa39ed79f1d7e4bf78b67ad3e4725ab801ad2f1b
JDucommun666 <jon.ducommun@gmail.com>
2014-09-10 13:25:50 -0500

    textbook work 4-Sep

---
+++ Chapter 7/maxof3.py
@@ -0,0 +1,31 @@
+__author__ = 'JDucommun'
+
+def main():
+    x1, x2, x3 = eval(input("Please enter three values"))
+    if x1 > x2
+        print("First number is greater than second")
+        if x1 > x3
+            print("First number is greater than third, too!")
+            print()
+
+
+def each2all():
+    if x1 >= x2 and x1 >= x3:
+        max = x1
+    elif x2 >= x1 and x2 >= x3:
+        max = x2
+    else:
+        max = x3
+
+def dectree():
+    if x1 >= x2:
+        if x1 >= x3:
+            max = x1
+        else:
+            max = x3
+    else:
+        if x2 >= x3:
+            max = x2
+        else:
+            max = x3
+
